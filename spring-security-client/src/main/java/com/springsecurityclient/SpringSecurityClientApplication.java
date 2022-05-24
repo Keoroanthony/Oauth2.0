12a3fe7047b6821f105cbe32d4ec8c23a5d64dd1
Keoroanthony <anthony.keoro@gmail.com>
2022-05-24 12:27:24 +0300

    commit uno

---
+++ spring-security-client/src/main/java/com/springsecurityclient/SpringSecurityClientApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurityclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringSecurityClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityClientApplication.class, args);
+    }
+}
